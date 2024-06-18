2c44a22b0c26cd7efddbd4d1a3862e10fac0b900
ncip48 <mbahcip00@gmail.com>
2024-06-18 14:41:07 +0700

    rev: redirect

--- kavling/views.py
+++ kavling/views.py
@@ -12,7 +12,8 @@ import re
 # Create your views here.
 # custom 404 view
 def custom_404(request, exception):
-    return render(request, '404.html', status=404)
+    # return render(request, '404.html', status=404)
+    return redirect('dashboard')
 
 @login_required
 # @permission_required('dashboard.index')
--- kavlingapp/settings.py
+++ kavlingapp/settings.py
@@ -32,8 +32,7 @@ SECRET_KEY = 'django-insecure-2@hb1oq-ufy!f*08yjb-^4^@b62$sk%kx^&^q)jwz#*g48r$z2
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
